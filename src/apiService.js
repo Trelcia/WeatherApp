da2deb3efc95fd5022438f1e88fcadf370ea80e3
Natalia Trelak <natalia@dascompany.com>
2023-06-29 15:09:59 +0200

    Added request chain handling

---
+++ src/apiService.js
@@ -0,0 +1,12 @@
+export const getWeatherByCity = city => {
+    return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},pl&appid=413dd34c402051f171bc44dc76b25d22`)
+    .then(resp => resp.json())
+    .then(data => data.main)
+    .then(weather => {
+        const temp = Math.round(weather.temp - 273.15) + '°C'
+        const tempMax = Math.round(weather.temp_max - 273.15) + '°C'
+        const tempMin = Math.round(weather.temp_min - 273.15) + '°C'
+
+        return (`${temp} ${tempMax} ${tempMin}`)
+    })
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-"use strict"
+import { getWeatherByCity } from './apiService.js';
 
 const viewElems = {}
 
@@ -9,7 +9,7 @@ const conectHTMLElems = () => {
     viewElems.weatherSearchView = getDOMElem('weatherSearchView');
     viewElems.weatherForecastView = getDOMElem('weatherForecastView');
 
-    viewElems.serchInput = getDOMElem('serchInput');
+    viewElems.searchInput = getDOMElem('searchInput');
     viewElems.searchButton = getDOMElem('searchButton');
 
     viewElems.weatherCityContainer = getDOMElem('weatherCityContainer');
@@ -21,19 +21,25 @@ const conectHTMLElems = () => {
     viewElems.weatherMinTemp = getDOMElem('weatherMinTemp');
 
     viewElems.returnToSearchBtn = getDOMElem('returnToSearchBtn');
-};
-const setUpListeners = () => {
-    viewElems.serchInput.addEventListener('keydown', onEnterSubmit);
-    viewElems.serchButton.addEventListener('click', onClickSubmit);
+}
+
+const setupListeners = () => {
+    viewElems.searchInput.addEventListener('keydown', onEnterSubmit);
+    viewElems.searchButton.addEventListener('click', onClickSubmit);
 }
 
 const initializeApp = () => {
     conectHTMLElems();
-    setUpListeners()
+    setupListeners()
 }
 
+const onEnterSubmit = event => {
+    if(event.key === 'Enter') {
+        let query = viewElems.searchInput.value;
+        getWeatherByCity(query)
+        .then(data => console.log(data));
+    }
+}
 const onClickSubmit = () => {};
-const onEnterSubmit = () => {};
 
-
-document.addEventListener('DOMContentLoaded', initializeApp);
+document.addEventListener('DOMContentLoaded', initializeApp); 
