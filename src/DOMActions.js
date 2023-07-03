451cddf23230ad816aa24578b84f3ab1680776e6
Natalia Trelak <natalia@dascompany.com>
2023-07-03 12:13:57 +0200

    Switched to class version of an app, did cleanup, created DOMActions module

---
+++ src/DOMActions.js
@@ -0,0 +1,17 @@
+const _getDOMElem = id => {
+    return document.getElementById(id);
+}
+
+const mapListToDOMElems = listOfId => {
+    const _viewElems = {}
+
+    for (const id of listOfId) {
+        _viewElems[id] = _getDOMElem(id);
+    }
+
+    return _viewElems
+}
+
+export {
+    mapListToDOMElems
+} 
